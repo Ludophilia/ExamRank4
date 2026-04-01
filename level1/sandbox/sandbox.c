0087c31fdf291b589efc9038d19bb17428317871
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2026-04-01 02:19:12 +0200

    Sandbox: forgot return -1 at line 26/27

--- level1/sandbox/sandbox.c
+++ level1/sandbox/sandbox.c
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 20:40:16 by jegerman          #+#    #+#             */
-/*   Updated: 2026/04/01 02:16:19 by jegerman         ###   ########.fr       */
+/*   Updated: 2026/04/01 02:18:30 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int	sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 	int		wstatus;
 
 	if (f == NULL)
+		return (-1);
 	pid = fork();
 	if (pid == -1)
 		return (-1);
