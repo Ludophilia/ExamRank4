4480881617ae091aec7384b6ba3c36368186a022
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2026-06-08 19:49:03 +0200

    Popen: Minor refactoring and stylistic improvements.

--- level1/ft_popen/v2/ft_popen.c
+++ level1/ft_popen/v2/ft_popen.c
@@ -6,43 +6,42 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 20:29:35 by jegerman          #+#    #+#             */
-/*   Updated: 2026/06/07 21:57:35 by jegerman         ###   ########.fr       */
+/*   Updated: 2026/06/08 19:40:29 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <sys/types.h>
-#include <stdarg.h>
 #include <stdlib.h>
 
 // const applies to the keyword on the left, or in the right if there's nothing.
 int	ft_popen(char const *file, char *const argv[], char type)
 {
-	int		pp[2], r_mode;
+	int		pp[2], type_r;
 	pid_t	pid;
 
-	if (((r_mode = (type == 'r')) || type == 'w') == 0
-		|| file == NULL || argv == NULL || *argv == NULL
+	if (((type_r = (type == 'r')) || type == 'w') == 0
+		|| file == NULL
+		|| argv == NULL || *argv == NULL
 		|| pipe(pp) == -1)
 		return (-1);
 	if ((pid = fork()) == -1)
 		return (close(pp[0]), close(pp[1]), -1);
 	if (pid == 0)
 	{
-		if ((r_mode? close(pp[0]): close(pp[1])) == -1
-			|| (r_mode? dup2(pp[1], 1): dup2(pp[0], 0)) == -1
-			|| (r_mode? close(pp[1]): close(pp[0])) == -1
+		if ((type_r? close(pp[0]): close(pp[1])) == -1
+			|| (type_r? dup2(pp[1], 1): dup2(pp[0], 0)) == -1
+			|| (type_r? close(pp[1]): close(pp[0])) == -1
 			|| execvp(file, argv) == -1)
 		{
-			close(pp[0]);
-			close(pp[1]);
+			(void)(close(pp[0]), close(pp[1]));
 			exit(1);
 		}
 		exit(0);
 	}
-	if ((r_mode? close(pp[1]): close(pp[0])) == -1)
+	if ((type_r? close(pp[1]): close(pp[0])) == -1)
 		return (close(pp[0]), close(pp[1]), -1);
-	return (r_mode? pp[0]: pp[1]);
+	return (type_r? pp[0]: pp[1]);
 }
 
 // int	ft_popen(char const *file, char *const argv[], char type)
