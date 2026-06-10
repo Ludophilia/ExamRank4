105c743cafe3d6c92863596543e31a2358450286
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2026-06-10 21:31:46 +0200

    Picoshell: started rewriting every lines related to those pesky close syscalls. Removed the use of helpers like close_all or smart_close. Properly isolated the edge cases this time... Not over yet but we're on the right path this time...

--- level1/picoshell/v2/picoshell.c
+++ level1/picoshell/v2/picoshell.c
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 01:11:36 by jegerman          #+#    #+#             */
-/*   Updated: 2026/06/09 21:29:13 by jegerman         ###   ########.fr       */
+/*   Updated: 2026/06/10 21:26:25 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,28 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 
-int	smrt_close(int *fd)
-{
-	close(*fd);
-	*fd = 0;
-	return (1);
-}
+// int	smrt_close(int *fd)
+// {
+// 	close(*fd);
+// 	*fd = 0;
+// 	return (1);
+// }
 
-int close_all(int pi[2], int prev_rpi)
-{
-	if (pi[0] != 0 && pi[0] != prev_rpi)
-		close(pi[0]);
-	if (prev_rpi != 0)
-		close(prev_rpi);
-	if (pi[1] != 0)
-		close(pi[1]);
-	return (1);
-}
+// int close_all(int pi[2], int prev_rpi)
+// {
+// 	if (pi[0] != 0 && pi[0] != prev_rpi)
+// 		close(pi[0]);
+// 	if (prev_rpi != 0)
+// 		close(prev_rpi);
+// 	if (pi[1] != 0)
+// 		close(pi[1]);
+// 	return (1);
+// }
 
 // 8/06. I want to get rid of those close all and smart close functions...
 int	picoshell(char **cmds[])
 {
-	int		wstatus, pi[2] = {0, 0}, prev_rpi = 0;
+	int		wstatus, pi[2], lpi0 = 0; // 0 init will be removed.
 	pid_t	pid;
 
 	if (cmds == NULL)
@@ -45,86 +45,60 @@ int	picoshell(char **cmds[])
 	// {---(3)>cmds[1]<(6)---pipe} 
 	// {---(5)>cmds[2]...(1)}
 
-	// TWO FDS... That's all there is to manage.
-	//		- But there's more.
-	//			- Those two fds have to be managed on two levels.
-
 	for (int i = 0; cmds[i]; i++)
 	{
 		// In parent
-
-		if (cmds[i][0] == NULL)
-		{
-			// How much is i?
-				// first or i == 0, no pipe have been opened yet, nothing to close, return (1);
-				// last or cmds[i + 1] == NULL, there's only the read end to close.
-				// else,
-					// there's only the read end as well. No new pipe have been 
-					// opened yet.
-			return (close_all(pi, prev_rpi), 1); // BULLSHIT. Poorly thought out...
-		}
-
-		prev_rpi = (i > 0 ? pi[0]: 0);
-		
-		if (cmds[i + 1] != NULL && pipe(pi) == -1 && close_all(pi, prev_rpi))
-			return (1);
-
-	
-		pid = fork();
-		if (pid == -1 && close_all(pi, prev_rpi))
-			return (1);
-
+		if (cmds[i][0] == NULL || (cmds[i + 1] && pipe(pi) == -1))
+			return (i && close(lpi0), 1);
+		if ((pid = fork()) == -1)
+			return (i && close(lpi0), close(pi[0]), close(pi[1]), 1);
 
 		// In children
 		if (pid == 0)
 		{
-
-			// The problem (most of the time) in children:
-				// - One opened pipe (both directions)
-				// - One reference to a previous pipe.
-
-			// always close read end of pipe (if exists) you definitely don't need 
-			if (pi[0] != 0 && pi[0] != prev_rpi)
-				smrt_close(pi + 0);
+			// 10/06: Improvements are still possible
 			
+			// Why here? Cue: cmds[i + 1] is also below...
+			if (cmds[i + 1] && close(pi[0]) == -1)
+				(void)(i && close(lpi0), close(pi[1]), exit(1));
 
-			if (prev_rpi != 0)
+			if (i)
 			{
-				// What does remain at that point? Not ALL...
-				if (dup2(prev_rpi, 0) == -1 && close_all(pi, prev_rpi))
-					exit(1);
-				smrt_close(&prev_rpi); // close previous read end of the pipe (if exists)
-			}
-			if (cmds[i + 1] != NULL)
-			{
-				// What does remain at that point? Not ALL...
-				if (dup2(pi[1], 1) == -1 && close_all(pi, prev_rpi))
-					exit(1);
-				smrt_close(pi + 1); // close write end of pipe (if exists) once dup2'd 
+				if (dup2(lpi0, 0) == -1)
+					(void)(i && close(lpi0), close(pi[1]), exit(1));
+				if (close(lpi0) == -1)
+					(void)(close(pi[1]), exit(1));
 			}
 
+			if (cmds[i + 1])
+			{
+				if (dup2(pi[1], 1) == -1)
+					(void)(close(pi[1]), exit(1));
+				if (close(pi[1]) == -1)
+					exit(1);
+			}
 		
-			// What's still open at that point? Nothing. So why close_all?
 			if (execvp(cmds[i][0], cmds[i]) == -1)
 				exit(1);
-
-			// We'll never see past this... EITHER execvp suceed and the prog
-			// is executed, OR execvp fail, and exit(1) is executed.
 		}
 
 		// In parent (again)
 
-		// Always close the write end of the pipe after use in children.
-		if (pi[1] != 0)
+		// 10/06... This takes time... TO a later date then.
+
+		if (cmds[i + 1] && close(pi[i]) == -1)
 			smrt_close(pi + 1);
 		// Leave the read end open. Close the PREVIOUS read end.
-		if (prev_rpi != 0)
-			smrt_close(&prev_rpi);
+		if (i)
+			smrt_close(&lpi0);
 
 		// Close_all? Besides the read end (if exists), what do you even close?
 		// At that level, there's only the read end (if exits) that's still there.
 		if (wait(&wstatus) == -1 || WEXITSTATUS(wstatus) == 1)
-			return (close_all(pi, prev_rpi), 1); // Bullshit, poorly thought out...
+			return (close_all(pi, lpi0), 1); // Bullshit, poorly thought out...
+
+		// The only one that remains open... The read end.
+		lpi0 = pi[0];
 	}
 	return (0);
 }
