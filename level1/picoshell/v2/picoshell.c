cc34f9aa228461b56dc48ec39a30815cedcf20f5
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2026-06-16 19:31:24 +0200

    Picoshell: Minor changes: typos, variables names...

--- level1/picoshell/v2/picoshell.c
+++ level1/picoshell/v2/picoshell.c
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 01:11:36 by jegerman          #+#    #+#             */
-/*   Updated: 2026/06/16 18:11:13 by jegerman         ###   ########.fr       */
+/*   Updated: 2026/06/16 19:26:02 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ typedef enum e_flg
 {
 	P0 = (1 << 0), P1 = (1 << 1), P0L = (1 << 2),
 	P0P1 = (P0 | P1),
-	ALL = (P0 | P1 | P0L),	
+	PALL = (P0 | P1 | P0L),
 }	t_flg;
 
 typedef struct s_fds
@@ -33,7 +33,7 @@ typedef struct s_fds
 
 int	close_all(int flags, t_fds *f, int i, char **cmds[])
 {
-	if ((flags & P0L) && (i > 0))
+	if ((flags & P0L) && i > 0)
 		close(f->lpi0);
 	if ((flags & P0) && cmds[i + 1])
 		close(f->pi[0]);
@@ -46,7 +46,7 @@ int	picoshell(char **cmds[])
 {
 	t_fds		f;
 	pid_t		pid;
-	int			wstatus, werrors = 0;;
+	int			wstatus, werrors = 0;
 
 	for (int i = 0; cmds[i]; i++)
 	{
@@ -54,11 +54,11 @@ int	picoshell(char **cmds[])
 			return (CL(P0L), 1);
 		pid = fork();
 		if (pid == -1)
-			return (CL(ALL), 1);
+			return (CL(PALL), 1);
 		if (pid == 0)
 		{
 			if (i > 0 && dup2(f.lpi0, 0) == -1)
-				CLEX(ALL, 1);
+				CLEX(PALL, 1);
 			if (i > 0 && close(f.lpi0) == -1)
 				CLEX(P0P1, 1);
 			if (cmds[i + 1] && close(f.pi[0]) == -1)
@@ -74,7 +74,7 @@ int	picoshell(char **cmds[])
 			return (CL(P0), 1);
 		if (cmds[i + 1] && close(f.pi[1]) == -1)
 			return (CL(P0 | P0L), 1);
-		f.lpi0 = (cmds[i + 1]? f.pi[0]: -1);
+		f.lpi0 = cmds[i + 1]? f.pi[0]: -1;
 	}
 	for (int j = 0; cmds[j]; j++)
 		if (wait(&wstatus) == -1 || WEXITSTATUS(wstatus) == 1)
