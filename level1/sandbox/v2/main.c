9e48cacd9e88e2c475af93dbd189ef3e4b459aec
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2026-06-26 15:10:09 +0200

    Sandbox: Added extra test cases. Everything seems fine. Moving on to microshell, that will be the last one after quick review of those level 2 assignments.

--- level1/sandbox/v2/main.c
+++ level1/sandbox/v2/main.c
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 20:10:13 by jegerman          #+#    #+#             */
-/*   Updated: 2026/06/25 23:10:12 by jegerman         ###   ########.fr       */
+/*   Updated: 2026/06/26 15:07:36 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,7 @@ void	test_sigint(void) { raise(SIGINT); }
 void	test_sigquit(void) { raise(SIGQUIT); }
 
 void	test_sigpipe(void) { int fds[2]; pipe(fds); close(fds[0]); 
-	 write(fds[1], "42", 2); write(1, "Do you see that line (spoiler: no)? \n", 15);  
-	/* close(fds[1]); (useless) */ }
+	 write(fds[1], "42", 2); /* close(fds[1]); */ }
 
 void	test_segfault(void) { *(int *)0 = 42; }
 
@@ -40,7 +39,7 @@ void	test_exit_0(void) { exit(0); }
 
 void	test_timeout(void) { pause(); }
 
-void	test_evil_timeout(void) { signal(SIGALRM, SIG_IGN); /* pwnd ;)*/ pause(); }
+void	test_evil_timeout(void) { signal(SIGALRM, SIG_IGN); pause(); }
 
 void	test_sigstop(void) { raise(SIGSTOP); }
 
@@ -51,30 +50,24 @@ int	main(void)
 {
 	int	sandbox(void (*)(void), unsigned int, bool);
 
-	// Goal: Maintain timeout functionality while avoiding sigalarm deactivation in
-	// the child... 
-
 	printf("== TEST NICE ==\n");
-	printf("-> sandbox exit: %i\n", sandbox(test_exit_0, 3, true));
-
+	printf("(sandbox exit: %i)\n", sandbox(test_exit_0, 3, true));
+	printf("== TEST EXIT ==\n");
+	printf("(sandbox exit: %i)\n", sandbox(test_exit_42, 3, true));
 	printf("== TEST TIMEOUT ==\n");
-	printf("-> sandbox exit: %i\n", sandbox(test_timeout, 3, true));
-
+	printf("(sandbox exit: %i)\n", sandbox(test_timeout, 3, true));
 	printf("== TEST EVIL TIMEOUT ==\n");
-	printf("-> sandbox exit: %i\n", sandbox(test_evil_timeout, 5, true));
-
+	printf("(sandbox exit: %i)\n", sandbox(test_evil_timeout, 5, true));
 	printf("== TEST SIGSTOP ==\n");
-	printf("-> sandbox exit: %i\n", sandbox(test_sigstop, 3, true));
-
+	printf("(sandbox exit: %i)\n", sandbox(test_sigstop, 3, true));
 	printf("== TEST SIGTSTP ==\n");
-	printf("-> sandbox exit: %i\n", sandbox(test_sigtstp, 3, true));
-
+	printf("(sandbox exit: %i)\n", sandbox(test_sigtstp, 3, true));
+	printf("== TEST SEGFAULT ==\n");
+	printf("(sandbox exit: %i)\n", sandbox(test_segfault, 1, true));	
 	printf("== TEST SIGABORT ==\n");
-	printf("-> sandbox exit: %i\n", sandbox(test_sigabort, 1, true));
-
+	printf("(sandbox exit: %i)\n", sandbox(test_sigabort, 1, true));
 	printf("== TEST SIGFPE ==\n");
-	printf("-> sandbox exit: %i\n", sandbox(test_sigfpe, 7, true));
-
+	printf("(sandbox exit: %i)\n", sandbox(test_sigfpe, 7, true));
 	printf("== TEST SIGPIPE ==\n");
-	printf("-> sandbox exit: %i\n", sandbox(test_sigpipe, 7, true));
+	printf("(sandbox exit: %i)\n", sandbox(test_sigpipe, 7, true));
 }
