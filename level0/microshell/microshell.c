e561f45c847b2e4f11942e4684e0a344cab098fc
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2026-07-02 22:08:46 +0200

    Microshell: Added \";\" management and a very brief draft of cd. That part is functional but minute error handling is missing. Started dividing logic into different functions.

--- level0/microshell/microshell.c
+++ level0/microshell/microshell.c
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/26 16:00:16 by jegerman          #+#    #+#             */
-/*   Updated: 2026/06/30 21:00:04 by jegerman         ###   ########.fr       */
+/*   Updated: 2026/07/02 22:04:41 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,28 @@
 #include <wait.h>
 #include <string.h>
 
+// 30/06: Error handling is missing...
+
+
 // 30/06: Builtin cd management is missing...
 
-// 30/06: Error handling is missing...
+int	builtin_cd(char *path)
+{
+	// changes directory
+	chdir(path);
+}
+
+
+// 30/06: ';' management is missing for now...
+
+// WHat's for?
+
+// Separates pipelines.
+//	sleep 10 | sleep 11 ; sleep 12
+//		sleep 10 and 11 are executed first in parallel, THEN sleep 12 alone
+// sleep 10 | echo a ; echo b
+//		sleep 10 and echo a are executed first in parallel. a is printed on
+// 		stdout. b is printed once the pipeline is done.
 
 char **create_cmd(char **argv, int *i, int *cmd_nb)
 {
@@ -41,77 +60,87 @@ char **create_cmd(char **argv, int *i, int *cmd_nb)
 	return (*i += size, *cmd_nb += 1, cmd);
 }
 
+int exec_cmd(int *pi, char *cur_arg, char **cmd, int cmds_nb, char **envp) // Norm!! (5 args)
+{
+	pid_t	pid;
+
+	if ((pid = fork()) == -1)
+		return (-1);
+	if (pid == 0)
+	{
+		if (cur_arg && strncmp(cur_arg, ";", 2) != 0)
+			close(pi[0]);
+		if (cmds_nb > 1)
+		{
+			dup2(pi[2], 0);
+			close(pi[2]);
+		}
+		if (cur_arg && strncmp(cur_arg, ";", 2) != 0)
+		{
+			dup2(pi[1], 1);
+			close(pi[1]);
+		}
+		execve(*cmd, cmd, envp);
+		exit(1);
+
+	}
+	if (cur_arg && strncmp(cur_arg, ";", 2) != 0)
+		close(pi[1]);
+	if (cmds_nb > 1)
+		close(pi[2]);
+
+	return (0);
+}
+
+int	exec_pipeline(int *i, int argc, char **argv, char **envp)
+{
+	int		pi[3];
+	int		cmds_nb;
+	char	**cmd;
+	int		wstatus;
+
+	cmds_nb = 0;
+	while (*i < argc && strncmp(argv[*i], ";", 2) != 0)
+	{
+		
+		if ((cmd = create_cmd(argv + *i, i, &cmds_nb)) == NULL)
+			return (-1);
+
+		if (argv[*i] && strncmp(argv[*i], "|", 2) == 0)
+			(void)(pipe(pi), *i += 1);
+	
+		exec_cmd(pi, argv[*i], cmd, cmds_nb, envp);
+
+		free(cmd);
+		pi[2] = argv[*i]  && strncmp(argv[*i] , ";", 2) != 0 ? pi[0] : -1;
+	}
+	for (int j = 0; j < cmds_nb; j++)
+		waitpid(-1, &wstatus, 0);
+	return (0);
+}
+
 // $>./microshell /bin/ls "|" /usr/bin/grep microshell ";" /bin/echo i love my microshell
 // microshell
 // i love my microshell
+
+
+// ./microshell /usr/bin/sleep 10 "|" /usr/bin/echo a ";" /usr/bin/echo b
+
+// ./microshell /usr/bin/echo a ";" /usr/bin/echo b
+// ./microshell /usr/bin/echo a ";"
+
 int microshell(int argc, char **argv, char **envp)
 {	
-	int 	wstatus;
-	pid_t 	pid;
-
-	int 	pi[2];
-	int		last_pi0;
-	char 	**cmds;
-
 	int 	i;
-	int		cmds_nb;
 
 	i = 0;
-	cmds_nb = 0;
 
-	// 30/06: ';' management is missing for now...
 	while (i < argc)
 	{
-		// printf("[1] i -> %i / argc -> %i\n", i, argc);
-		// printf("%s\n", argv[i]);
-		// fflush(NULL);
-
-		if ((cmds = create_cmd(argv + i, &i, &cmds_nb)) == NULL)
-			return (-1);
-
-		// printf("[2] i -> %i / argc -> %i\n", i, argc);
-		// printf("%s\n", argv[i]);
-		// fflush(NULL);
-			
-		if (argv[i] && strncmp(argv[i], "|", 2) == 0)
-			(void)(pipe(pi), i++); // ???
-
-		if ((pid = fork()) == -1)
-			return (-1);
-
-		if (pid == 0)
-		{
-			if (argv[i])
-				close(pi[0]);
-
-			if (cmds_nb > 1)
-			{
-				dup2(last_pi0, 0);
-				close(last_pi0);
-			}
-			if (argv[i])
-			{
-				dup2(pi[1], 1);
-				close(pi[1]);
-			}
-
-			execve(*cmds, cmds, envp); // char **cmds
-			exit(1);
-		}
-
-		free(cmds);
-		
-		if (argv[i])
-			close(pi[1]);
-		if (cmds_nb > 1)
-			close(last_pi0);
-		
-
-		last_pi0 = argv[i] ? pi[0] : -1;
+		exec_pipeline(&i, argc, argv, envp);
+		if (argv[i] && strncmp(argv[i], ";", 2) == 0)
+			i++;
 	}
-
-	for (int j = 0; j < cmds_nb; j++)
-		waitpid(pid, &wstatus, 0);
 
 	return (0);
 }
