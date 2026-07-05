4f3f5a2eb104bab22a3fa56252413f2b36cd8e78
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2026-07-05 20:17:09 +0200

    Microshell: Minor modifications on the different function interfaces (return values, function arguments...) to make the incrementation logic more explicit. Done some testing as well. I hope what comes next will be easier because I'm really spending time on details right here...

--- level0/microshell/microshell.c
+++ level0/microshell/microshell.c
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/26 16:00:16 by jegerman          #+#    #+#             */
-/*   Updated: 2026/07/04 23:09:40 by jegerman         ###   ########.fr       */
+/*   Updated: 2026/07/05 20:14:26 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ typedef struct s_cnt
 {
 	int		cmds;
 	int		pi;
+	int		toks;
 }	t_cnt;
 
 // 3/07: Error handling is incomplete...
@@ -32,9 +33,8 @@ int	builtin_cd(char *path)
 	chdir(path);
 }
 
-char **create_cmd(char **toks, t_cnt *ct, int *i)
+int	create_cmd(char **toks, char ***cmd, t_cnt *ct)
 {
-	char	**cmd;
 	int		size;
 	int		j;
 
@@ -43,14 +43,14 @@ char **create_cmd(char **toks, t_cnt *ct, int *i)
 		&& strncmp(toks[size], "|", 2) != 0
 		&& strncmp(toks[size], ";", 2) != 0)
 		size++;
-	cmd = malloc((size + 1) * sizeof(char *));
-	if (cmd == NULL)
+	*cmd = malloc((size + 1) * sizeof(char *));
+	if (*cmd == NULL)
 		return (-1);
 	j = -1;
 	while (++j < size)
-		cmd[j] = toks[j];
-	cmd[j] = 0;
-	return (ct->cmds += 1, *i += size, cmd);
+		(*cmd)[j] = toks[j];
+	(*cmd)[j] = 0;
+	return (ct->cmds += 1, ct->toks = size);
 }
 
 int exec_cmd(char **cmd, t_cnt *ct, int *pi, char **envp)
@@ -61,15 +61,9 @@ int exec_cmd(char **cmd, t_cnt *ct, int *pi, char **envp)
 		return (-1);
 	if (pid == 0)
 	{
-
-		// if there's a new pipe just opened before? (That's it?)
 		if (ct->pi > 0)
 			close(pi[0]);
 
-		// int cmds_nb -> helps understanding the command's position in the 
-		// pipeline and the necessity to use last pipe's read end at pi[2]
-		// if there's at least two commands, meaning there are two pipes opened,
-		// the first one likely only with its read end...
 		if (ct->cmds > 1)
 		{
 			dup2(pi[2], 0);
@@ -80,13 +74,11 @@ int exec_cmd(char **cmd, t_cnt *ct, int *pi, char **envp)
 			dup2(pi[1], 1);
 			close(pi[1]);
 		}
-	
+
 		execve(*cmd, cmd, envp);
 		free(cmd);
 		exit(1);
-
 	}
-
 
 	if (ct->pi > 0)
 		close(pi[1]);
@@ -96,9 +88,8 @@ int exec_cmd(char **cmd, t_cnt *ct, int *pi, char **envp)
 	return (0);
 }
 
-int	exec_pipeline(char **toks, char **envp)
+int	exec_pipeline(char **toks, char **envp, int *toks_usd)
 {
-	// (LESS) BLOATED
 	char	**cmd;
 	int		pi[3];
 	int		wstatus;
@@ -110,32 +101,26 @@ int	exec_pipeline(char **toks, char **envp)
 	ct.pi = 0;
 	while (toks[i] && strncmp(toks[i], ";", 2) != 0)
 	{
-		// Current: Pipeline with 1 command, no pipe. What's supposed to happen.
-	
-		if ((cmd = create_cmd(toks + i, &ct, &i)) == NULL)
+		if (create_cmd(toks + i, &cmd, &ct) == -1)
 			return (-1);
-
-
+		i += ct.toks;
 		if (toks[i] && strncmp(toks[i], "|", 2) == 0)
 		{
-			pipe(pi);
-			i++;
+			if (pipe(pi) == -1) // error handling: use picoshell's solution
+				return (ct.cmds > 1 && close(pi[2]), -1);
 			ct.pi++;
+			i++;
 		}
-		
 		exec_cmd(cmd, &ct, pi, envp);
-	
-
-		free(cmd);		
-		pi[2] = ct.pi > 0 ? ct.pi--, pi[0] : -1;
+		free(cmd);
+		pi[2] = ct.pi > 0 ? (ct.pi--, pi[0]) : -1;
 	}
 	
 	// wstatus error handling...
 	for (int j = 0; j < ct.cmds; j++)
 		waitpid(-1, &wstatus, 0);
 
-	// Reminder: We're supposed to return the number of token consumed...
-	return (0);
+	return (*toks_usd = i);
 }
 
 
@@ -148,17 +133,20 @@ int	exec_pipeline(char **toks, char **envp)
 // ./microshell /usr/bin/echo a ";" /usr/bin/echo b
 // ./microshell /usr/bin/echo a ";"
 
+// valgrind --track-fds=yes ./microshell /usr/bin/last 
+// "|" /usr/bin/head -20 "|" /usr/bin/head "|" /usr/bin/nl "|" /usr/bin/tac ";"
+
 int microshell(char **toks, char **envp)
 {
 	int		i;
-	int		toks_usd;
+	t_cnt	ct;
 
 	i = 0;
 	while (toks[i])
 	{
-		if ((toks_usd = exec_pipeline(toks + i, envp)) == -1)
+		if (exec_pipeline(toks + i, envp, &ct.toks) == -1)
 			return (-1);
-		i += toks_usd;
+		i += ct.toks;
 		if (toks[i] && strncmp(toks[i], ";", 2) == 0)
 			i++;
 	}
